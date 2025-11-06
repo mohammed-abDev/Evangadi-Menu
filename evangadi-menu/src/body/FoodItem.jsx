20dae6b853a9d505fea9921a6b8bcde3efd1e5cd
mohammed-abDev <mohammedabdudev11@gmail.com>
2025-11-06 11:29:14 +0300

    Created FoodItem component

--- evangadi-menu/src/body/FoodItem.jsx
+++ evangadi-menu/src/body/FoodItem.jsx
@@ -7,7 +7,18 @@ import menu from "../commonResource/data.js";
     return (
       <>
         <div className={styles["foods-container"]}>
-          
+          {menu.map(({ id, title, category, price, img, desc }) => (
+            <div key={id} className={styles["single-food"]}>
+              <div className={styles.img}>
+                <img src={img} alt={title} />
+              </div>
+              <div className={styles["title-price"]}>
+                <h3>{title}</h3>
+                <p>{price}</p>
+              </div>
+              <div className={styles["food-desc"]}>{desc}</div>
+            </div>
+          ))}
         </div>
       </>
     );
